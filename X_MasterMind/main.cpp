7d24e72dc5a266cc46a5fdd90f318273a123aa54
Lasagnezio0 <150262556+Lasagnezio0@users.noreply.github.com>
2024-03-23 11:42:40 +0000

    Mastermind EZ

--- X_MasterMind/main.cpp
+++ X_MasterMind/main.cpp
@@ -5,6 +5,13 @@ int main(int argc, char const *argv[])
 {
     masterMind prova = masterMind();
     
-    prova.readNewMove();
+    std::cout << "Benvenuto al gioco di mastermind!" << std::endl;
+    
+    do{
+        prova.readNewMove();
+        prova.move_result();
+    }while(prova.can_continue());
+
+    prova.ending_situation();
     return 0;
 }
--- X_MasterMind/masterMind.cpp
+++ X_MasterMind/masterMind.cpp
@@ -11,6 +11,7 @@ private:
     static const int DIM_VALID_MOVE = 4;
     int valid_move[DIM_VALID_MOVE];
     int secret_code[DIM_VALID_MOVE];
+    bool victory;
 
     bool sanitize_input(){
         if(DIM_VALID_MOVE != last_move.size()) return false;
@@ -31,21 +32,64 @@ private:
         srand(time(NULL));
         for(int i = 0; i < DIM_VALID_MOVE; i++){
             secret_code[i] = rand()% 10;
+            //std::cout << secret_code[i];
         }
     }
 
-    void check(){
-        int ball = 0;
-        int strike = 0;
-        int copy_secret[DIM_VALID_MOVE];
+    void print_secret(){
+        for(int i = 0; i < DIM_VALID_MOVE; i++){
+            std::cout << secret_code[i];
+        }
+        std::cout << std::endl;
+    }
+    
+    int* copia_vettore(int og_vettore[]){
+        
+        static int copy_vect[DIM_VALID_MOVE]; // static avvisa che la parte di memoria del vettore non venga cancellata una volta finito il metodo
         //copio il vettore risposta affinchè esso non venga modificato
-        for(int z = 0; z < DIM_VALID_MOVE; z++){
-            copy_secret[z] = secret_code[z];
+        
+        for(int i = 0; i < DIM_VALID_MOVE; i++){
+            copy_vect[i] = og_vettore[i];
         }
         
+        return copy_vect;
+    }
+
+    void copia_vettore_bis(int vettore_originale[], int vettore_copia[], int dim_vet){
+        for(int i = 0; i < DIM_VALID_MOVE; i++){
+            vettore_copia[i] = vettore_originale[i];
+        }
+    }
+
+
+public:
+
+    masterMind(){
+        n_moves = 0;
+        last_move = "";
+        victory = false;
+        generate_secret_code();
+    };
+
+    void readNewMove(){
+        do{
+            std::cout << "tentativo " <<  n_moves + 1 << ": ";
+            std::getline(std::cin, last_move); //getline legge anche gli spazi, si ferma a leggere quando incontra un a capo 
+        }while(!sanitize_input());
+
+        n_moves++;
+    }
+
+    void move_result(){
+        int ball = 0;
+        int strike = 0;
+
+        int *copy_secret = copia_vettore(secret_code);
+
         // controllo ball e strike
         for(int j = 0; j < DIM_VALID_MOVE; j++){
             for(int i = 0; i < DIM_VALID_MOVE; i++){
+                
                 if(valid_move[i] == copy_secret[i]){
                     strike++;
                     copy_secret[i] = -2;
@@ -54,30 +98,26 @@ private:
                 if(valid_move[j] == copy_secret[i]){
                     ball++;
                     copy_secret[i] = -2;
+                    continue;
                 }
             }
         }
-        std::cout << "strike[" << strike << "]          ball[" << ball << "]" << std::endl;
+
+        if(strike == DIM_VALID_MOVE) victory = true;
+
+        std::cout << "Here your strike [" << strike << "] and ball [" << ball << "]" << std::endl;
     }
 
-public:
-
-    masterMind(){
-        n_moves = 0;
-        last_move = "";
-    };
-
-    void readNewMove(){
-        do{
-            std::cout << n_moves + 1 << ": ";
-            std::getline(std::cin, last_move); //getline legge anche gli spazi, si ferma a leggere quando incontra un a capo 
-        }while(!sanitize_input());
-
-        n_moves++;
+    bool can_continue(){
+        if(victory == true || n_moves >= 9) return false;
+        return true;
     }
 
-    void move_result(){
-        
+    void ending_situation(){
+        print_secret();
+        if(victory == true) std::cout << "Hai vinto!" << std::endl;
+        else std::cout << "Mi dispiace, hai perso!" << std::endl;
     }
+    
 };
 
