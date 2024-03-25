5f7a8390177c4fb09266546d083e2dbafa0c8c48
Lasagnezio0 <150262556+Lasagnezio0@users.noreply.github.com>
2024-03-25 08:23:07 +0000

    SommaRicorsivaStrana

---
+++ Z_VARI/SONG/esercizi in classe/SommaTraDueInteri.cpp
@@ -0,0 +1,30 @@
+/*
+* Dondi Alessandro
+** 25/3/2024 3CIN 
+*/
+
+#include <iostream>
+
+int sommaRicorsiva(int n1, int n2){
+    do{
+        if(n1 == n2) break;
+        return n1 + sommaRicorsiva(n1 + 1, n2);
+    }while(n1 != n2);
+}
+
+int main(){
+    int m, n, somma;
+    m = n = somma = 0;
+    
+    
+    std::cout << "attenzione: il primo numero deve essere minore del secondo" << std::endl;
+    std::cout << "Inserisci il primo numero: " << std::endl;
+    std::cin >> m;
+    std::cout << "Inserisci il secondo numero: " << std::endl;
+    std::cin >> n;
+
+    somma = sommaRicorsiva(m,n);
+    std::cout << "La somma tra i numeri compresi tra i due numeri è " << somma << std::endl;
+    
+    return 0;
+}
